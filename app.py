7484ef8be0f01169bf65699bd35c7621d3487124
LukeMKwaka494 <lucasmalala4@gmail.com>
2025-04-25 13:49:46 +1000

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+st.title("**Talking Text: PNG Text-to-Speech**")
+
+st.markdown("""
+**Talking Text** is a simple and free app that turns your words into spoken voice.
+
+- Supports **English** and **Tok Pisin**
+- Built for **Papua New Guinea**
+- Easy to use on phones
+""")
